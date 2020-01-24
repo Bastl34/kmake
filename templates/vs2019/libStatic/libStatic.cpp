1168cc69ac205c956f613681240795e133db007c
Bastian Karge <bastian.karge@gmail.com>
2020-01-24 01:24:55 +0100

    added vs2019 templates (without cleanup at the moment)

---
+++ templates/vs2019/libStatic/libStatic.cpp
@@ -0,0 +1,10 @@
+// libStatic.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
+void fnlibStatic()
+{
+}
