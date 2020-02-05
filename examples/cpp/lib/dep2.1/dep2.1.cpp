aea7dab02045c1f4e2082596b79c8e4dc3ae481b
Bastian Karge <bastian.karge@gmail.com>
2020-02-05 18:11:03 +0100

    some renaming

--- examples/cpp/lib/dep2.1/dep2.1.cpp
+++
@@ -1,6 +0,0 @@
-#include "dep2.1.h"
-
-void dep21Func()
-{
-    std::cout << "hello from dep2.1" << std::endl;
-}
---
+++ examples/cpp/lib/dep21/dep21.cpp
@@ -0,0 +1,6 @@
+#include "dep21.h"
+
+void dep21Func()
+{
+    std::cout << "hello from dep21" << std::endl;
+}
